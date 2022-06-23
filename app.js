ebad9bdc07578bf5d318babacd7c939a24dfd6f8
mackgabe <mackenzieabe@gmail.com>
2022-06-23 10:52:22 -0400

    added generate page function and page template js

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.lo 
+const fs=require('fs');
+const generatePage= require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github]= profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML= generatePage(name, github);
 
 
-const printProfileData = profileDataArr => {
-    //this....
-    for (let i=0; i < profileDataArr.length; i+= 1) {
-        console.log(profileDataArr[i]);
-    }
+fs.writeFile('./index.html', generatePage(name,github),err => {
+    if (err) throw new Error(err);
 
-    console.log('===========')
+    console.log('Portfolio complete! Check out index.html to see the output!');
+ });
 
-    //is the same as this....
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+
+
+// const printProfileData = profileDataArr => {
+//     //this....
+//     for (let i=0; i < profileDataArr.length; i+= 1) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('===========')
+
+//     //is the same as this....
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
        
-    };
+//     };
 
   
-  printProfileData(profileDataArgs);
+//   printProfileData(profileDataArgs);
