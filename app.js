fc577fe6e7717729fd20cff8b95a519f8a29bce2
mackgabe <mackenzieabe@gmail.com>
2022-06-26 09:26:54 -0400

    added inquirer prompt

--- app.js
+++ app.js
@@ -1,37 +1,22 @@
-const fs=require('fs');
-const generatePage= require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+.prompt([
+    {
+        type:'input',
+        name:'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers=>console.log(answers));
 
-console.log(profileDataArgs);
+// const fs=require('fs');
+// const generatePage= require('./src/page-template.js');
 
-const [name, github]= profileDataArgs;
+// const pageHTML= generatePage(name, github);
 
-console.log(name, github);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-const pageHTML= generatePage(name, github);
-
-
-fs.writeFile('./index.html', generatePage(name,github),err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
- });
-
-
-
-// const printProfileData = profileDataArr => {
-//     //this....
-//     for (let i=0; i < profileDataArr.length; i+= 1) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log('===========')
-
-//     //is the same as this....
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-       
-//     };
-
-  
-//   printProfileData(profileDataArgs);
+//     console.log('Portfolio complete! Check out the index.html to see the output!');
+// });
